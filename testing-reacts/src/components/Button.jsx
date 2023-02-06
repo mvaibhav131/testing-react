31b12b1fa2d42ae693772c09f78322392f98d72f
“mvaibhav131” <98808183+mvaibhav131@users.noreply.github.com>
2023-02-06 17:03:50 +0530

    testcasesforbutton

--- testing-reacts/src/components/Button.jsx
+++ testing-reacts/src/components/Button.jsx
@@ -1,7 +1,13 @@
+import "./button.css";
 
-import "./button.css"
-
-export const Button= ({variant,colorScheme,children,onClick})=>{
-    return <button onClick={onClick} className={`btn ${colorScheme || "red"} ${variant || "ghost"}`} >{children}</button>
+export const Button = ({ variant, colorScheme, children, onClick }) => {
+  return (
+    <button
+      data-testid="cButton"
+      onClick={onClick}
+      className={`btn ${colorScheme || "red"} ${variant || "ghost"}`}
+    >
+      {children}
+    </button>
+  );
 };
-
--- testing-reacts/src/components/Button.test.jsx
+++ testing-reacts/src/components/Button.test.jsx
@@ -1,10 +1,17 @@
 import { render ,screen } from "@testing-library/react";
+import App from "../App"
 import { Button } from "./Button";
 
 describe("Button Test", () => {
     test("should have button in the dom", ()=> {
       render(<Button>Click me</Button>)
+    //   screen.debug();
       let button = screen.getByText("Click me");
       expect(button).toBeInTheDocument;
     });
+     test("should render based on testid",()=> {
+        render(<Button>Click me</Button>)
+        let button = screen.getByTestId("cButton");
+        expect(button).toBeInTheDocument
+     })
 });
