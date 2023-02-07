0bee5a44734ae3693e19d782660b9cd0f02d01ba
“mvaibhav131” <98808183+mvaibhav131@users.noreply.github.com>
2023-02-07 15:43:36 +0530

    test cases

--- testing-reacts/src/components/Button.test.jsx
+++ testing-reacts/src/components/Button.test.jsx
@@ -1,4 +1,5 @@
-import { render ,screen } from "@testing-library/react";
+import { fireEvent, render ,screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event"
 import App from "../App"
 import { Button } from "./Button";
 
@@ -9,9 +10,29 @@ describe("Button Test", () => {
       let button = screen.getByText("Click me");
       expect(button).toBeInTheDocument;
     });
+    
      test("should render based on testid",()=> {
         render(<Button>Click me</Button>)
         let button = screen.getByTestId("cButton");
         expect(button).toBeInTheDocument
+     });
+
+     test("should render the empty button",()=> {
+      render(<Button></Button>)
+      let button =screen.getByTestId("cButton")
+      expect(button).toBeEmptyDOMElement();
+     })
+
+     test("should change theme",()=>{
+      render(<App></App>)
+      let h3= screen.getByText("Theme is light")
+      expect(h3).toBeInTheDocument();
+      expect(h3).toHaveTextContent("light");
+
+      let themeButton = screen.getByText("Change Theme");
+      // userEvent.click(themeButton) 
+      fireEvent.click(themeButton)
+      expect(h3).toHaveTextContent("dark");
+
      })
 });
