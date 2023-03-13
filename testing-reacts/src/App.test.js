375ff821a87f2fcb99b1237b09a519d2d880fe2b
“mvaibhav131” <98808183+mvaibhav131@users.noreply.github.com>
2023-03-13 11:41:41 +0530

    app test comment

--- testing-reacts/src/App.test.js
+++ testing-reacts/src/App.test.js
@@ -1,6 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+// these is already in the app
+
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText("Button testing in react");
